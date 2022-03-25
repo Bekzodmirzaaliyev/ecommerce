6703f388232328cccb00b542b534022002a15b14
Bekzod Mirzaaliyev <Bekzodmirzaaliyev27@gmail.com>
2022-03-25 06:48:32 +0500

    ASDDDDD

--- server/app.js
+++ server/app.js
@@ -36,8 +36,13 @@ const customizeRouter = require("./routes/customize");
 const { loginCheck } = require("./middleware/auth");
 
 //config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "../env" });
+if(process.env.NODE.ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  });
+  
 }
 
 // Database Connection
